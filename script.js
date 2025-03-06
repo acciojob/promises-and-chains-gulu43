651741f7f34a610f0769712a08a60bd2ec091ecd
Gulamnabi Kagathala <145833927+gulu43@users.noreply.github.com>
2025-03-06 01:57:15 -0300

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,44 @@
-//your JS code here. If required.
-    let form = document.getElementById("votingForm");
-
-    form.addEventListener("submit", function (event) {
-        event.preventDefault();
-
-        let name = document.getElementById("name").value.trim();
-        let age = document.getElementById("age").value.trim();
-        if (name === "" || age === "") {
+    let age_var = document.querySelector("#age");
+    let name_var = document.querySelector("#name");
+    let btn = document.querySelector("#btn");
+    let form = document.querySelector("#f");
+    
+    form.addEventListener('submit',(e)=>{
+        e.preventDefault();
+        if(age_var.value == "" || name_var.value == ""){
+            // throw new Error("age or name can not be empty");
             alert("Please enter valid details.");
-            return;
+			return;
         }
+        // console.log(age_var.value, name_var.value);
+        varify_fn(age_var.value,name_var.value)
+        .then((message)=>{
+            alert(message);
+        })
+        .catch((message)=>{
+            alert(message);
+        })
 
-        age = Number(age);
+        
+    });
 
-        let votingPromise = new Promise((resolve, reject) => {
-            setTimeout(() => {
-                if (age > 18) {
-                    resolve(Welcome, ${name}. You can vote.);
-                } else {
-                    reject(Oh sorry ${name}. You aren't old enough.);
+    let varify_fn = (age_v,name_v)=>{
+        let promise_obj = new Promise((resolve,reject)=>{
+            setTimeout(()=>{
+                if (age_v>18) {
+                    return resolve(`Welcome, ${name_v}. You can vote.`);
+                }else{
+                    return reject(`Oh sorry ${name_v}. You aren't old enough.`);
                 }
-            }, 4000); // 4-second delay
+            },3000);
         });
-		
-        votingPromise
-            .then((message) => alert(message))
-            .catch((errorMessage) => alert(errorMessage));
-    });
+        return promise_obj;
+
+        // return new Promise((resolve,reject)=>{
+        //     if (age_v>18) {
+        //         return resolve("Welcome, . You can vote.",name_v);
+        //     }else{
+        //         return reject("Oh sorry . You aren't old enough.",name_v);
+        //     }
+        // })
+    }
