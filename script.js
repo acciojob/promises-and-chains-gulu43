10b93bfe2282477f78bce05edbc33714ee960e25
Gulamnabi Kagathala <145833927+gulu43@users.noreply.github.com>
2025-03-06 02:05:54 -0300

    created a submission

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ form.addEventListener('submit', (e) => {
     e.preventDefault();
 
     if (age_var.value.trim() === "" || name_var.value.trim() === "") {
-        alert("Please enter valid details.");
-		console.log('Please enter valid details.');
+        alert("Please enter valid details");
+		console.log('Please enter valid details');
         return;
     }
 
