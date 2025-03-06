29fab61224fbc79e78370fe92d93b83fd9a8fa88
Gulamnabi Kagathala <145833927+gulu43@users.noreply.github.com>
2025-03-06 01:59:30 -0300

    created a submission

--- script.js
+++ script.js
@@ -1,44 +1,33 @@
-    let age_var = document.querySelector("#age");
-    let name_var = document.querySelector("#name");
-    let btn = document.querySelector("#btn");
-    let form = document.querySelector("#f");
-    
-    form.addEventListener('submit',(e)=>{
-        e.preventDefault();
-        if(age_var.value == "" || name_var.value == ""){
-            // throw new Error("age or name can not be empty");
-            alert("Please enter valid details.");
-			return;
-        }
-        // console.log(age_var.value, name_var.value);
-        varify_fn(age_var.value,name_var.value)
-        .then((message)=>{
-            alert(message);
-        })
-        .catch((message)=>{
-            alert(message);
-        })
+let age_var = document.querySelector("#age");
+let name_var = document.querySelector("#name");
+let btn = document.querySelector("#btn");
+let form = document.querySelector("#f");
 
-        
-    });
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
 
-    let varify_fn = (age_v,name_v)=>{
-        let promise_obj = new Promise((resolve,reject)=>{
-            setTimeout(()=>{
-                if (age_v>18) {
-                    return resolve(`Welcome, ${name_v}. You can vote.`);
-                }else{
-                    return reject(`Oh sorry ${name_v}. You aren't old enough.`);
-                }
-            },3000);
-        });
-        return promise_obj;
-
-        // return new Promise((resolve,reject)=>{
-        //     if (age_v>18) {
-        //         return resolve("Welcome, . You can vote.",name_v);
-        //     }else{
-        //         return reject("Oh sorry . You aren't old enough.",name_v);
-        //     }
-        // })
+    if (age_var.value.trim() === "" || name_var.value.trim() === "") {
+        alert("Please enter valid details.");
+        return;
     }
+
+    verify_fn(age_var.value, name_var.value)
+        .then((message) => {
+            alert(message);
+        })
+        .catch((message) => {
+            alert(message);
+        });
+});
+
+let verify_fn = (age_v, name_v) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (age_v > 18) {
+                resolve(`Welcome, ${name_v}. You can vote.`);
+            } else {
+                reject(`Oh sorry ${name_v}. You aren't old enough.`);
+            }
+        }, 3000);
+    });
+};
